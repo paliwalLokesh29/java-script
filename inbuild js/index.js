54c3d8c5abcdba41e670ac1db8c660b935c54f3b
Lokesh paliwal <lp1296974@gmail.com>
2026-03-28 09:57:06 +0530

    inbuild js

---
+++ inbuild js/index.js
@@ -0,0 +1,22 @@
+// console.log("love");
+
+// console.log(Math.PI);
+
+// console.log(Math.max(8,5,4,7,1,2,3,4,5,6)); //8
+// console.log(Math.min(8,5,4,7,1,2,3,4,5,6)); //1
+
+// console.log(Math.round(1.6)); //2
+// console.log(Math.floor(1.1)); //1
+// console.log(Math.ceil(1.1)); //2
+
+// console.log(Math.abs(-5));  //5
+
+// console.log(Math.random());
+// console.log(Math.sqrt(4));
+// console.log(Math.pow(2,10));
+
+let curr = new Date();
+
+console.log(curr);
+
+
